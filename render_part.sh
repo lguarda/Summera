20f6dc66104fdb7c0218b75da312fe1eaf15232d
lguarda <leoman037@live.fr>
2025-03-19 20:17:46 +0100

    openscad: fix render_part script

--- render_part.sh
+++ render_part.sh
@@ -1,1 +1,5 @@
-for i in `perl -ne '/part\("(.*?)"\)/ && print("$1\n")' $1`; do openscad -DPART="\"$i\"" -o "./$(basename --suffix=".scad" $1)-$i.stl" $i; done
+#!/usr/bin/env bash
+
+for i in `perl -ne '/part\("(.*?)"\)/ && print("$1\n")' $1`; do
+    openscad -DPART="\"$i\"" -o "./$(basename --suffix=".scad" $1)-$i.stl" $1;
+done
